447fc56275f5327a37ff0b544154728b8ba01b2b
Leslie Dancsecs <lesdancsecs@gmail.com>
2025-07-17 11:47:57 -0300

    Updated boolean count example.

--- example/booleanCount/main.go
+++ example/booleanCount/main.go
@@ -1,4 +1,6 @@
 // Package main implements a simple example of using szargs.
+//
+//nolint:forbidigo // OK to print to os.Stdout.
 package main
 
 import (
@@ -15,15 +17,15 @@ func main() {
 	)
 
 	howMany := args.Count(
-		"[-c || --count ...]", // Short and long forms both repeatable.
-		"How many times?)",
+		"[-c | --count ...]", // Short and long forms both repeatable.
+		"How many times?",
 	)
 
 	args.Done() // All arguments should have consumed.
 
-	_, _ = fmt.Fprintf(os.Stdout, "How many: %d\n", howMany)
-
 	if args.HasError() {
-		_, _ = fmt.Fprintln(os.Stderr, "Error: "+args.Err().Error())
+		fmt.Fprintf(os.Stderr, "Error: %v\n\n%s\n", args.Err(), args.Usage())
+	} else {
+		fmt.Printf("How many: %d\n", howMany)
 	}
 }
